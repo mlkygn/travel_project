c229d03e725d208d5bba714a0165df0e66deccfc
Melike Y <melike@diji.tech>
2024-01-19 23:03:44 +0300

    animations + responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./home.css";
 import video from "../../assets/video.mp4";
+
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { FaFacebook } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -8,62 +9,85 @@ import { AiFillInstagram } from "react-icons/ai";
 import { FaListUl } from "react-icons/fa";
 import { RiApps2Line } from "react-icons/ri";
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function Home() {
+  useEffect(() => {
+    Aos.init(
+      {
+        duration: 2000,
+      },
+      []
+    );
+  });
   return (
     <section className="home">
       <div className="overlay"></div>
       <video src={video} muted autoPlay loop typeof="video/mp4"></video>
 
       <div className="container">
-        <div className="search-text">
+        <div className="search-text" data-aos="fade-up">
           <h2>Our Packages</h2>
           <h1>Search Your Holiday</h1>
         </div>
         <div className="search-card">
           <form>
-            <label htmlFor="">Search your destination:</label>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter Destination"
-              />
-              <HiOutlineLocationMarker className="input-icon" />
+            <div className="row">
+              <div className="col-md-6 col-lg-4">
+                <label htmlFor="">Search your destination:</label>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Destination"
+                  />
+                  <HiOutlineLocationMarker className="input-icon" />
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4">
+                <label htmlFor="">Select your date:</label>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control"
+                    placeholder="Enter Date"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4">
+                <label htmlFor="">
+                  Max price: <span className="max-price">$5000</span>
+                </label>
+                <div className="form-group">
+                  <input
+                    type="range"
+                    className="form-range"
+                    min="1000"
+                    max="5000"
+                    placeholder="Enter Time"
+                  />
+                </div>
+              </div>
             </div>
-            <label htmlFor="">Select your date:</label>
-            <div className="form-group">
-              <input
-                type="date"
-                className="form-control"
-                placeholder="Enter Date"
-              />
-            </div>
-            <label htmlFor="">
-              Max price: <span className="max-price">$5000</span>
-            </label>
-            <div className="form-group">
-              <input
-                type="range"
-                className="form-range"
-                min="1000"
-                max="5000"
-                placeholder="Enter Time"
-              />
-            </div>
+
             <button type="submit" className="btn btn-linear">
               Search
             </button>
           </form>
         </div>
-        <div className="d-flex mt-5 fs-2 justify-content-between text-white">
+        <div
+          className="d-flex mt-5 fs-2 justify-content-between text-white"
+          data-aos="fade-up"
+        >
           <div>
             <FaFacebook className="me-2" />
-            <FaSquareXTwitter className="me-2"/>
+            <FaSquareXTwitter className="me-2" />
             <AiFillInstagram />
           </div>
           <div>
-          <FaListUl className="me-2" />
-          <RiApps2Line />
+            <FaListUl className="me-2" />
+            <RiApps2Line />
           </div>
         </div>
       </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./main.css";
 import Data from "./Data";
 
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { GrNext } from "react-icons/gr";
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function Main() {
+  useEffect(() => {
+    Aos.init(
+      {
+        duration: 2000,
+      },
+      []
+    );
+  });
   return (
     <section className="main container py-5">
-      <h3>
+      <h3 data-aos="fade-right">
         Most visited <span className="underline">destinations</span>
       </h3>
       <div className="row gy-3">
         {Data.map((item) => {
           return (
-            <div className="col-12">
-              <div key={item.id} className="card">
+            <div className="col-12 col-md-6 col-lg-4 col-xl-3">
+              <div key={item.id} className="card"  data-aos="fade-up">
                 <img src={item.img} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h4 className="card-title fw-bold fs-4">{item.title}</h4>
@@ -37,7 +48,9 @@ function Main() {
                   <div className="fs-14 text-muted fw-medium">
                     {item.descriptions}
                   </div>
-                  <div className="btn btn-linear mt-3">DETAILS <GrNext className="fs-5" /></div>
+                  <div className="btn btn-linear mt-3">
+                    DETAILS <GrNext className="fs-5" />
+                  </div>
                 </div>
               </div>
             </div>
